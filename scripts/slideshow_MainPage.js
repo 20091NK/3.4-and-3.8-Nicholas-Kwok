c9d55dc69b1a9757cfe25c10f7c586fdff8c43b7
20091NK <125605149+20091NK@users.noreply.github.com>
2024-07-22 22:53:23 +0000

    Development of Main Page: Web - Conversion of Sidebar Slideshow to Manual Navigation

--- scripts/slideshow_MainPage.js
+++ scripts/slideshow_MainPage.js
@@ -1,18 +1,27 @@
-var slideIndexA = 0;
-showSlides()
+var slideIndexA = 1;
+showSlides(slideIndexA);
 
+function changeSlide(n) {
+  showSlides(slideIndexA += n);
+}
 
-function showSlides() {
+function currentSlide(n) {
+  showSlides(slideIndexA = n);
+}
+
+function showSlides(n) {
   let i;
   let x = document.getElementsByClassName("SlidesA");
-
+  let y = document.getElementsByClassName("BadgeA");
+  if (n > x.length) { slideIndexA = 1 }
+  if (n < 1) { slideIndexA = x.length }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  for (i = 0; i < x.length; i++) {
+    y[i].className = y[i].className.replace(" w3-white", "");
+  }
 
-  slideIndexA++;
-  if (slideIndexA > x.length) { slideIndexA = 1 }
   x[slideIndexA - 1].style.display = "block";
-
-  setTimeout(showSlides, 5000);
+  y[slideIndexA - 1].className += " w3-white";
 }
