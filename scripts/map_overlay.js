af65fb45cd5849da4b2a3e9fe5d89dbb3626de9f
20091NK <125605149+20091NK@users.noreply.github.com>
2024-08-20 22:35:36 +0000

    Development of Main Page: VR & Web - Adjusted Overlay/VR switching

--- scripts/map_overlay.js
+++ scripts/map_overlay.js
@@ -1,10 +1,12 @@
 let map = document.getElementById("map-overlay");
 let scene = document.getElementById("scene-overlay");
+let modal = document.getElementById("mapoverlay-modal");
 showOverlay()
 
 
 function hideOverlay() {
   map.style.display = "none";
+  modal.style.display = "none";
   scene.style.display = "block";
 }
 
